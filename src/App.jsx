742dba53618998bab8a1e4dc991b2e0610c6a1c3
pleochroism <pleochroism@gmail.com>
2020-11-10 00:01:14 +0900

    YouTube polpolar Video List with Title

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+import React,{ useState, useEffect } from 'react';
+import './App.css';
+import VideoList from './components/video_list/video_list';
+
+function App() {
+  const [videos, setVideos] = useState([]);   
+  useEffect(() => {
+    const requestOptions = {
+      method: 'GET',
+      redirect: 'follow'
+    };    
+    fetch("https://www.googleapis.com/youtube/v3/videos?part=snippet&chart=mostPopular&maxResults=25&key=AIzaSyD8xWMPpsYqBo5KIAsSqZ6GhkROmGPdaHI", requestOptions)
+      .then(response => response.json())
+      .then(result =>setVideos(result.items))
+      .catch(error => console.log('error', error));    
+    }, [])
+    return <VideoList videos={videos} />
+}
+
+export default App;
